f267b7d7e1f7a1683522e8c8084f0ab3a0ad55b4
xchen <xiaoling.chen@uth.tmc.edu>
2012-02-21 15:40:59 -0600

    change the protocol files under protocols dir

--- Betsy/protocol_engine.py
+++ Betsy/protocol_engine.py
@@ -5,12 +5,18 @@ import rule_engine
 import argparse
 import os
 
+def import_protocol(protocol):
+    protocol_name = 'protocols.'+protocol
+    mod = __import__(protocol_name)
+    mod = getattr(mod, protocol)
+    return mod
+
 def filter_pipelines(protocol, inputs, output_info,
                      in_dataset_ids, in_contents, parameters=None):
     """given the Inputs and Output and Parameters dictionary,
        return a list of pipelines,if Parameters is None,
        then use the default parameters"""
-    module = __import__(protocol)
+    module = import_protocol(protocol)
     pl_inputs = []
     assert len(in_dataset_ids) == len(inputs)
     assert len(in_contents) == len(inputs)
@@ -44,7 +50,7 @@ def run_protocol(protocol, inputs, output_info, identifiers,
        return a list of final result file,
       if Parameters is None, then use the default parameters"""
     
-    module = __import__(protocol)
+    module = import_protocol(protocol)
     pipelines = filter_pipelines(protocol, inputs, output_info,
                                  in_dataset_ids, in_contents, parameters)
     pl_inputs = []
@@ -92,8 +98,8 @@ def main():
     args = parser.parse_args()
     if not args.protocol_file:
         raise ValueError('please specify the protocol_file')
-    
-    module = __import__(args.protocol_file)
+  
+    module = import_protocol(args.protocol_file)
     inputs = []
     identifiers = []
     in_dataset_ids = []
---
+++ Betsy/protocols/normalize_file.py
@@ -0,0 +1,52 @@
+#normalize_file.py
+
+INPUTS = [
+    ['gse_dataset'],
+    ['gse_dataset','class_label_file'],
+    ['gse_dataset','gene_list_file'],
+    ['gse_dataset_and_platform'],
+    ['gse_dataset_and_platform','class_label_file'],
+    ['gse_dataset_and_platform','gene_list_file'],
+    ['geo_dataset'],
+    ['geo_dataset','class_label_file'],
+    ['geo_dataset','gene_list_file'],
+    ['input_signal_file'],
+    ['input_signal_file','class_label_file'],
+    ['input_signal_file','gene_list_file']]
+
+OUTPUTS = ['signal_file']
+
+PARAMETERS = {
+    'preprocess':['rma','mas5','loess','illumina_controls',
+                  'ilumina','agilent','unknown_preproces'],
+    'gene_center':['mean','median','no_gene_center'],
+    'gene_normalize':['variance','sum_of_squares','no_gene_normalize'],
+    'quantile':['yes_quantile','no_quantile'],
+    'dwd':['yes_dwd','no_dwd'],
+    'shiftscale':['yes_shiftscale','no_shiftscale'],
+    'combat':['yes_combat','no_combat'],
+    'gene_order':['by_sample_ttest','by_gene_list','no_order'],
+    'filter':['no_filter','integer between 0 and 100']}
+
+DEFAULT = {
+    'preprocess':'rma','gene_center':'no_gene_center',
+    'gene_normalize':'no_gene_normalize','quantile':'no_quantile',
+    'dwd':'no_dwd','shiftscale':'no_shiftscale',
+    'combat':'no_combat','filter':'no_filter',
+    'gene_order':'no_order'}
+
+predicate2arguments={
+    'gse_dataset':([],'[]'),
+    'gse_dataset_and_platform':([],'[]'),
+    'geo_dataset':(['version','unknown_version'],'[]'),
+    'class_label_file':(['status','given'],'[]'),
+    'gene_list_file':([],'[]'),
+    'input_signal_file':(['status','given'],'[]')}
+
+def format_prolog_query(
+    predicate,dataset_id,content,parameters,modules):
+    str_parameters = ','.join(parameters)
+    output = str('['+dataset_id+'],[' + content + '],[' +
+                 str_parameters + '],' + modules)
+    query = predicate + '(' + output+')'
+    return query
