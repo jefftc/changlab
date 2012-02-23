826d7aa6d9ed960d6ccb26b9d528ebf49fbbded3
xchen <xiaoling.chen@uth.tmc.edu>
2012-02-23 11:25:46 -0600

    fix the illumina and agilent modules

---
+++ Betsy/modules/extract_illumina_idat_files.py
@@ -0,0 +1,75 @@
+#extract_illumina_idat_files.py
+
+import module_utils
+import shutil
+import os
+import Betsy_config
+import zipfile
+import hash_method
+import rule_engine
+
+def extract_all(zipName):
+    z = zipfile.ZipFile(zipName)
+    for f in z.namelist():
+        if f.endswith('/'):
+            os.makedirs(f)
+        else:
+            z.extract(f)
+
+def run(parameters,objects,pipeline):
+    identifier,single_object = get_identifier(parameters,objects)
+    outfile,new_objects = get_outfile(parameters,objects)
+    if zipfile.is_zipfile(identifier):
+        directory = os.path.split(identifier)[-1]
+        directory = os.path.splitext(directory)[0]
+        directory = os.path.join(os.getcwd(),directory)
+        extract_all(identifier)
+    else:
+        directory = identifier
+    illumina_file = []
+    for filename in os.listdir(directory):
+        if filename in ['.DS_Store','._.DS_Store','.Rapp.history']:
+            continue
+        if filename.endswith('.idat'):
+            illumina_file.append(filename)
+    if illumina_file:
+        os.mkdir(outfile)
+        for filename in illumina_file:
+            old_file = os.path.join(directory,filename)
+            new_file = os.path.join(outfile,filename)
+            shutil.copyfile(old_file,new_file)
+        module_utils.write_Betsy_parameters_file(parameters,single_object,pipeline)
+        return new_objects
+    else:
+        return None
+    
+def make_unique_hash(parameters,objects):
+    identifier,single_object = get_identifier(parameters,objects)
+    hash_profile={'filename':os.path.split(identifier)[-1],
+                   'version': 'illumina',
+                   'number of files':str(len(os.listdir(identifier)))}
+    hash_result=hash_method.hash_parameters(**hash_profile)
+    return hash_result
+
+def get_outfile(parameters,objects):
+    identifier,single_object = get_identifier(parameters,objects)
+    old_filename = os.path.split(identifier)[-1]
+    if '_BETSYHASH1_' in old_filename: 
+        original_file = '_'.join(old_filename.split('_')[:-2])
+    else:
+        original_file = old_filename
+    hash_string = make_unique_hash(parameters,objects)
+    filename = original_file + hash_string
+    outfile = os.path.join(os.getcwd(),filename)
+    attributes = parameters.values()
+    objecttype = 'geo_dataset'
+    new_object = rule_engine.DataObject(objecttype,attributes,outfile)
+    new_objects = objects[:]
+    new_objects.remove(single_object)
+    new_objects.append(new_object)
+    return outfile,new_objects
+
+def get_identifier(parameters,objects):
+    return module_utils.find_object(parameters,objects,'geo_dataset','Contents,DatasetId')
+    
+
---
+++ Betsy/modules/preprocess_agilent.py
@@ -0,0 +1,54 @@
+#agilent.py
+import module_utils
+import shutil
+import os
+from genomicode import jmath
+
+def run(parameters,objects,pipeline):
+    identifier,single_object = get_identifier(parameters,objects)
+    outfile,new_objects = get_outfile(parameters,objects)
+    cwd = os.getcwd()
+    R = jmath.start_R()
+    R('library(marray)')
+    os.chdir(identifier)
+    try:
+        R('dir<-getwd()')
+        R('files<-list.files(dir)')
+        R('x.read<-read.Agilent(files)')
+    finally:
+        os.chdir(cwd)
+    R('xnorm.loc <- maNorm(x.read, norm = "loess")')
+    R('x.norm <- maNormScale(xnorm.loc, norm = "p")')
+    tmpfile = 'tmp.txt'
+    jmath.R_equals('\"' + tmpfile + '\"','tmpfile')
+    R('write.marray(x.norm,tmpfile)')
+    f=open(tmpfile,'r')
+    text=f.readlines()
+    firstline=text[0].split()
+    f.close()
+    firstindex = firstline.index('"ProbeName"')
+    secondindex = firstline.index('"Sequence"')
+    sample = range(secondindex+1,len(firstline))
+    f=open(outfile,'w')
+    for i in text:
+        line = i.split()
+        f.write(line[firstindex]+'\t')
+        for j in sample:
+            f.write(line[j]+'\t')
+        f.write('\n')
+    f.close()
+    os.remove(tmpfile)
+    module_utils.write_Betsy_parameters_file(
+    parameters,single_object,pipeline)
+    return new_objects
+    
+
+def make_unique_hash(parameters,objects):
+    return module_utils.make_unique_hash(parameters,objects,'geo_dataset','Contents,DatasetId')
+
+def get_outfile(parameters,objects):
+    return module_utils.get_outfile(parameters,objects,'geo_dataset','Contents,DatasetId','signal_file')
+    
+def get_identifier(parameters,objects):
+    return module_utils.find_object(parameters,objects,'geo_dataset','Contents,DatasetId')
+    
