6176d23e1869f0129a69d8323bde88b94e68b2f2
Jeffrey Chang <jeffrey.t.chang@uth.tmc.edu>
2012-07-26 14:38:06 -0500

    can now score multiple geneset files and expression data sets

--- scripts/score_geneset.py
+++ scripts/score_geneset.py
@@ -20,7 +20,7 @@ def parse_geneset(geneset):
         neg = x[1]
     return pos, neg
 
-def score_gene_set(MATRIX, name, pos_genes, neg_genes):
+def score_gene_set(MATRIX, matrix_name, name, pos_genes, neg_genes):
     from genomicode import matrixlib
     from genomicode import jmath
 
@@ -33,9 +33,8 @@ def score_gene_set(MATRIX, name, pos_genes, neg_genes):
     MATRIX_n = MATRIX.matrix(row=neg_genes, row_header=header)
 
     num_rows = MATRIX_p.nrow() + MATRIX_n.nrow()
-    x = "Gene set %s contains %d genes and matched %d rows in " + \
-        "the expression data."
-    print x % (name, len(all_genes), num_rows)
+    x = "Gene set %s contains %d genes and matched %d rows in %s."
+    print x % (name, len(all_genes), num_rows, matrix_name)
 
     X_p = MATRIX_p._X
     X_n = []
@@ -49,15 +48,18 @@ def score_gene_set(MATRIX, name, pos_genes, neg_genes):
 
 def main():
     import argparse
+    import glob
     
     parser = argparse.ArgumentParser(
         description="Score a gene set on a gene expression data set.")
 
-    parser.add_argument("geneset_file", help="File with gene sets.  "
-                        "Should be in gmx or gmt format.")
-    parser.add_argument("expression_file", help="Data set to score.")
+    parser.add_argument(
+        "expression_files", nargs="+", help="Data set(s) to score.")
 
     # Assumes that there are no commas in names of gene sets.
+    parser.add_argument(
+        "--geneset_file", dest="geneset_files", action="append", default=[],
+        help="File(s) with gene sets.  Should be in gmx or gmt format.")
     parser.add_argument(
         "-g", dest="gene_set", action="append", default=[],
         help="Name of the gene set to score.  If you want to score both "
@@ -72,12 +74,19 @@ def main():
         "-o", dest="outfile", default=None, help="Name of file for results.")
     
     args = parser.parse_args()
-    assert os.path.exists(args.geneset_file), \
-           "I could not find the gene set file: %s" % args.geneset_file
-    assert os.path.exists(args.expression_file), \
-           "I could not find the expression file: %s" % args.expression_file
+    assert args.expression_files, \
+           "Please specify an expression data set to score."
+    expression_files = []
+    for x in args.expression_files:
+        x = glob.glob(x)
+        expression_files.extend(x)
+    for x in expression_files:
+        assert os.path.exists(x), \
+           "I could not find the expression file: %s" % x
     assert args.outfile, "Please specify the name of an outfile."
-
+    assert args.geneset_files, "Please specify one or more geneset files."
+    for x in args.geneset_files:
+        assert os.path.exists(x), "I could not find the gene set file: %s" % x
     assert args.gene_set, "Please specify one or more gene sets to score."
 
     if args.libpath:
@@ -92,40 +101,64 @@ def main():
     
     genepattern.fix_environ_path()
 
-    print "Reading gene set file."; sys.stdout.flush()
+    msg = "Reading gene set file."
+    if len(args.geneset_files) > 1:
+        msg = "Reading gene set files."
+    print msg; sys.stdout.flush()
     genesets = {}  # name -> list of genes
-    for x in genesetlib.read_genesets(args.geneset_file):
-        name, description, genes = x
-        genesets[name] = genes
-    
-    print "Reading gene expression file."; sys.stdout.flush()
-    MATRIX = arrayio.read(args.expression_file)
+    for filename in args.geneset_files:
+        for x in genesetlib.read_genesets(filename):
+            name, description, genes = x
+            assert name not in genesets, "Duplicate geneset: %s." % name
+            genesets[name] = genes
 
-    results = []   # list of (geneset, list of scores)
+    NAMES = []
+    MATRICES = []
+    for filename in expression_files:
+        print "Reading gene expression file: %s." % filename
+        sys.stdout.flush()
+        x = os.path.split(filename)[1]
+        NAMES.append(x)
+        x = arrayio.read(filename)
+        MATRICES.append(x)
+
+    results = {}   # (matrix, geneset, sample) -> score
     for geneset in args.gene_set:
         pos_gs, neg_gs = parse_geneset(geneset)
         assert pos_gs in genesets, "I could not find gene set: %s" % pos_gs
         if neg_gs:
             assert neg_gs in genesets, "I could not find gene set: %s" % neg_gs
-        name = pos_gs
+        gs_name = pos_gs
         if neg_gs:
-            name = "%s/%s" % (pos_gs, neg_gs)
+            gs_name = "%s/%s" % (pos_gs, neg_gs)
             
         pos_genes = genesets[pos_gs]
         neg_genes = genesets.get(neg_gs, [])
-        scores = score_gene_set(MATRIX, name, pos_genes, neg_genes)
-        results.append((name, scores))
+
+        for matrix_name, MATRIX in zip(NAMES, MATRICES):
+            scores = score_gene_set(
+                MATRIX, matrix_name, gs_name, pos_genes, neg_genes)
+            sample_names = MATRIX.col_names(arrayio.COL_ID)
+            assert len(sample_names) == len(scores)
+            for (sample, score) in zip(sample_names, scores):
+                key = matrix_name, gs_name, sample
+                assert key not in results, "Duplicate: %s" % key
+                results[key] = score
+
+    x = [(x[0], x[2]) for x in results]
+    x = sorted({}.fromkeys(x))
+    all_matrix_samples = x
+    x = [x[1] for x in results]
+    x = sorted({}.fromkeys(x))
+    all_genesets = x
 
     outhandle = open(args.outfile, 'w')
-    sample_names = MATRIX.col_names(arrayio.COL_ID)
-    header = ["SAMPLE"] + [x[0] for x in results]
+    header = ["FILE", "SAMPLE"] + all_genesets
     print >>outhandle, "\t".join(header)
-    for i in range(len(sample_names)):
-        x = [sample_names[i]]
-        for (name, scores) in results:
-            assert len(scores) == len(sample_names), "%d %d" % (
-                len(scores), len(sample_names))
-            x.append(scores[i])
+    for x in all_matrix_samples:
+        matrix, sample = x
+        scores = [results[(matrix, x, sample)] for x in all_genesets]
+        x = [matrix, sample] + scores
         assert len(x) == len(header)
         print >>outhandle, "\t".join(map(str, x))
     outhandle.close()
