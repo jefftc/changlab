324386bef9f2bfce71f153d3f97f4301d36d23a0
xchen <xiaoling.chen@uth.tmc.edu>
2012-09-05 13:10:49 -0500

    update associate_gene_expression_with_outcomes.py

--- scripts/associate_gene_expression_with_outcomes.py
+++ scripts/associate_gene_expression_with_outcomes.py
@@ -5,68 +5,52 @@ import arrayio
 import os
 import argparse
 import numpy
-
+import sys
 def main():
-
     parser = argparse.ArgumentParser(
         description = 'associate the gene expression with outcomes')
-    parser.add_argument('-o',dest = 'outpath',type = str,
-                        help = 'specify the outpath',default=None)
-    parser.add_argument('-f',dest = 'expression_file',type = str,
-                        help = 'specify the gene expression file',default=None)
-    parser.add_argument('-cf',dest = 'clinical_data',type = str,
+    parser.add_argument(dest = 'expression_file',type = str,
+                        help = 'specify the gene expression data file', default=None)
+    parser.add_argument(dest = 'clinical_data',type = str,
                         help = 'specify the clinical data file', default=None)
-    parser.add_argument('--time_header',dest = 'time_header',type = str,
-                        help = 'specify the header for time column', default=None)
-    parser.add_argument('--dead_header',dest = 'dead_header',type = str,
-                        help = 'specify header for dead column', default=None)
+    parser.add_argument('--outcome',dest = 'outcome',type = str,
+                        help = 'specify time_header and dead_header in the format<time_header>,<dead_header>',default=[],
+                        action='append')
     parser.add_argument('--genes',dest = 'genes',type = str,
                         help = 'specify genes for analysis', default=None,
                         action='append')
+    parser.add_argument('-o',dest = 'outpath',type = str,
+                        help = 'specify the outpath',default=None)
     parser.add_argument('--prism_file',dest = 'prism_file',type = str,
                         help = 'specify the prism file name', default=None)
     args = parser.parse_args()
-    outfile = args.outpath
-    assert outfile,'please specify the path of output file'
     input_file = args.expression_file
     assert input_file,'please specify the path of gene expression data file'
     clinical_file = args.clinical_data
     assert clinical_file,'please specify the path of clinical data'
-    time_header = args.time_header
-    assert time_header,'please specify the header of month column in clinical data'
-    dead_header = args.dead_header
-    assert dead_header,'please specify the header of dead column in clinical data'
+    outcomes = args.outcome
+    assert len(outcomes)>0,'please specify the time_header and dead_header'
     genes = args.genes
     if genes:
         gene_list=[]
         for i in genes:
             gene_list.extend(i.split(','))
             
-    clinical_data = genesetlib.read_tdf(clinical_file,preserve_spaces=True, allow_duplicates=True)
-    time_data = None
-    dead_data = None
-    name_in_clinical = None
     M = arrayio.read(input_file)
+    clinical_data = genesetlib.read_tdf(clinical_file,preserve_spaces=True, allow_duplicates=True)
     sample_name = M._col_names['_SAMPLE_NAME']
-       
+    name_in_clinical = None
+    clinical_dict = dict()
     for g in clinical_data:
-        n = len(list(set(sample_name).intersection(set(g[2]))))
+        clinical_dict[g[0]] = g[2]
+    for g in clinical_dict:
+        n = len(list(set(sample_name).intersection(set(clinical_dict[g]))))
         if n > 0:
-           name_in_clinical = g[2]
-        if g[0] == time_header:
-            time_data = g[2]
-        if g[0] == dead_header:
-            dead_data = g[2]
+           name_in_clinical = clinical_dict[g]
     assert name_in_clinical,'cannot match the sample name in clinical data and gene expression data'
-    assert time_data,'there is no column named %s'%time_header
-    assert dead_data,'there is no column named %s'%dead_header
-    #only consider the sample who has month and dead information
-    sample_index1 = [index for index,item in enumerate(time_data) if len(item)>0]
-    sample_index2 = [index for index,item in enumerate(dead_data) if len(item)>0]
-    sample_index = list(set(sample_index1).intersection(set(sample_index2)))
     
     #align the sample order of gene expression file and clinical data
-    x= matrixlib.align_cols_to_annot(M,name_in_clinical,reorder_MATRIX=True)
+    x = matrixlib.align_cols_to_annot(M,name_in_clinical,reorder_MATRIX=True)
     M,colnames = x
     #if given genes,get the row index of match genes
     if genes:
@@ -81,52 +65,103 @@ def main():
         
     ids = M._row_order
     geneid = M._row_names[ids[0]]
+    headers=ids[:]
     data_all = M.slice()
-    output = []
-    R=jmath.start_R()
+    output_data = []
     for i in range(len(geneid)):
-        data = data_all[i]
-        data_new = [data[j] for j in sample_index]
-        median = numpy.median(data_new)
-        group1= [index for index,item in enumerate(data) if item>=median and index in sample_index]
-        group2= [index for index,item in enumerate(data) if item<median and index in sample_index]
-        group1_month=[float(time_data[k]) for k in group1]
-        group2_month=[float(time_data[k]) for k in group2]
-        survival1=numpy.mean(group1)
-        survival2=numpy.mean(group2)
-        group1_dead=[int(dead_data[k]) for k in group1]
-        group2_dead=[int(dead_data[k]) for k in group2]
-        med_high = numpy.median(group1_month)
-        med_low = numpy.median(group2_month)
-        jmath.R_equals(group1_month,'group1')
-        jmath.R_equals(group2_month,'group2')
-        jmath.R_equals(group1_dead,'dead1')
-        jmath.R_equals(group2_dead,'dead2')
-        R('source("'+config.kaplanmeierlib+'")')
-        R('a<-calc.km(group1,dead1,group2,dead2)')
-        c=R['a']
-        if c[0][0]<0.05:
-            if survival1>survival2:
-                direction = 'High gene expression correlates with high survival.'
-            else:
-                direction = 'High gene expression correlates with low survival.'
-        else:
-            direction = ''
-        output.append([str(c[0][0]),str(len(group2_month)),
-                       str(len(group1_month)),str(med_low),str(med_high),direction,str(median)])
+        output_data.append([M._row_names[name][i] for name in ids])
+        
+    R = jmath.start_R()
+    R('source("'+config.kaplanmeierlib+'")')
+    for outcome in outcomes:
+        time_data = None
+        dead_data = None
+        x = outcome.split(',')
+        assert len(x)==2, 'the outcome format should be <time_header>,<dead_header>'
+        time_header,dead_header = x
+        for g in clinical_dict:
+            if g == time_header:
+                time_data = clinical_dict[g]
+            if g == dead_header:
+                dead_data = clinical_dict[g]
+        assert time_data,'there is no column named %s'%time_header
+        assert dead_data,'there is no column named %s'%dead_header
+        #only consider the sample who has month and dead information
+        sample_index1 = [index for index,item in enumerate(time_data) if len(item)>0]
+        sample_index2 = [index for index,item in enumerate(dead_data) if len(item)>0]
+        sample_index = list(set(sample_index1).intersection(set(sample_index2)))
+        
+        for i in range(len(geneid)):
+            data = data_all[i]
+            data_new = [data[j] for j in sample_index]
+            data_order = data_new[:]
+            data_order.sort()
+            for percentage in [0.5,0.25,0.1]:
+                high_point = data_order[int(round(len(data_order)*(1-percentage)))]
+                low_point = data_order[int(round(len(data_order)*percentage))]
+                group1_index = [index for index,item in enumerate(data) if item>=high_point and index in sample_index]
+                group2_index = [index for index,item in enumerate(data) if item<low_point and index in sample_index]
+                group1_month=[float(time_data[k]) for k in group1_index]
+                group2_month=[float(time_data[k]) for k in group2_index]
+                group1_dead=[int(dead_data[k]) for k in group1_index]
+                group2_dead=[int(dead_data[k]) for k in group2_index]
+                jmath.R_equals(group1_month,'group1')
+                jmath.R_equals(group2_month,'group2')
+                jmath.R_equals(group1_dead,'dead1')
+                jmath.R_equals(group2_dead,'dead2')
+                R('a<-calc.km(group1,dead1,group2,dead2)')
+                c=R['a']
+                med_high_50 = c.rx2('surv1.50')[0]
+                med_high_90 = c.rx2('surv1.90')[0]
+                med_low_50 = c.rx2('surv2.50')[0]
+                med_low_90 = c.rx2('surv2.90')[0]
+                p_value = c.rx2('p.value')[0]
+                if p_value<0.05:
+                    if 'NA' not in [str(med_high_50),str(med_low_50)]:
+                        if med_high_50>med_low_50:
+                            direction = 'High gene expression correlates with high survival.'
+                        else:
+                            direction = 'High gene expression correlates with low survival.'
+                    elif  'NA' not in [str(med_high_90),str(med_low_90)]:
+                        
+                        if med_high_90>med_low_90:
+                            direction = 'High gene expression correlates with high survival.'
+                        else:
+                            direction = 'High gene expression correlates with low survival.'
+                    else:
+                        direction = ''
+                    
+                else:
+                    direction = ''
+                output_data[i].extend([str(p_value),str(len(group2_month)),
+                        str(len(group1_month)),str(med_low_50),str(med_high_50),str(med_low_90),
+                        str(med_high_90),direction,str(high_point)+'/'+str(low_point)])
+        newheaders = ['p(Outer 50%)','Num Patients Low(Outer 50%)','Num Patients High(Outer 50%)',
+                        '50% Survival Low(Outer 50%)','50% Survival High(Outer 50%)',
+                        '90% Survival Low(Outer 50%)','90% Survival High(Outer 50%)',
+                        'Relation(Outer 50%)','Cutoff(Outer 50%)',
+                      'p(Outer 25%)','Num Patients Low(Outer 25%)','Num Patients High(Outer 25%)',
+                        '50% Survival Low(Outer 25%)','50% Survival High(Outer 25%)',
+                        '90% Survival Low(Outer 25%)','90% Survival High(Outer 25%)',
+                        'Relation(Outer 25%)','Cutoff(Outer 25%)',
+                      'p(Outer 10%)','Num Patients Low(Outer 10%)','Num Patients High(Outer 10%)',
+                        '50% Survival Low(Outer 10%)','50% Survival High(Outer 10%)',
+                        '90% Survival Low(Outer 10%)','90% Survival High(Outer 10%)',
+                        'Relation(Outer 10%)','Cutoff(Outer 10%)']
+        if len(outcomes)>1:
+            newheaders = [time_header+' ' + i for i in newheaders]
+        headers.extend(newheaders)
         if args.prism_file:
             assert len(geneid)==1,'multiple genes match and cannot write the prism file'
             jmath.R_equals('"'+args.prism_file+'"','filename')
-            R('write.km.prism(filename,group1,dead1,group2,dead2)') 
-    f=file(outfile,'w')
-    headers=ids[:]
-    headers.extend(['p.50%','# patients low','# patients high','median survival low',
-                    'median survival high','Direction','Cutoff'])
-    f.write('\t'.join(headers)+'\n')
+            R('write.km.prism(filename,"High Expression",group1,dead1,"Low Expression",group2,dead2)')
+    
+    f = sys.stdout
+    if args.outpath: 
+        f = file(args.outpath,'w')
+    print >>f,'\t'.join(headers) 
     for i in range(len(geneid)):
-       descriptions = [M._row_names[name][i] for name in ids]
-       descriptions.extend(output[i])
-       f.write('\t'.join(descriptions)+'\n')
+       print >>f,'\t'.join(output_data[i])
     f.close()
                
 if __name__=='__main__':
