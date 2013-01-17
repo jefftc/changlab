c38f168b5a97ae4d88e1f748241d29b55aefdc79
xchen <xiaoling.chen@uth.tmc.edu>
2013-01-17 14:13:43 -0600

    fix run_genepattern.py bug

--- scripts/run_genepattern.py
+++ scripts/run_genepattern.py
@@ -26,8 +26,6 @@ def main():
             key,value = i.split(':')
             assert ':' not in value,'parameters should be in key:value format'
             parameters[key] = value
-    if not os.path.exists(args.outpath):
-        os.mkdir(args.outpath)
     """given the module_name and the module parameters
        in dict, call module in Genepatttern"""
     R = jmath.start_R()
@@ -40,17 +38,12 @@ def main():
     command = "\'" + args.module_name + "\'"
     for key in parameters.keys():
         command = command + ',' + key + '=' + '\"' + parameters[key] + '\"'
-    cwd = os.getcwd()
-    try:
-        os.chdir(args.outpath)
-        fullcommand = 'result<-run.analysis(gp.client,' + command + ')'
-        R('library(GenePattern)')
-        R('gp.client <- gp.login(servername, username, password)')
-        R(fullcommand)
-        jmath.R_equals(args.outpath,'outpath')
-        R('job.result.download.files(result, outpath)')
-    finally:
-        os.chdir(cwd)
+    fullcommand = 'result<-run.analysis(gp.client,' + command + ')'
+    R('library(GenePattern)')
+    R('gp.client <- gp.login(servername, username, password)')
+    R(fullcommand)
+    jmath.R_equals(args.outpath,'outpath')
+    R('job.result.download.files(result, outpath)')
 
 if __name__=='__main__':
     main()
