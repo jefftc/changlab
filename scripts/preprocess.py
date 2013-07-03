15b614d52e80385fe2498211e29794d7b6b63910
Jeffrey Chang <jeffrey.t.chang@uth.tmc.edu>
2013-07-03 09:57:11 -0500

    better error for preprocess

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 
-import os, sys
-from genomicode import arrayplatformlib
-
+import os
 
 def find_annotation_file(chipname):
-    from genomicode import config
+    from genomicode import arrayplatformlib
     chipname = chipname.replace('-','_')
     filename = arrayplatformlib.chipname2filename(chipname)
+    assert filename, "I could not find a file for chip: %s." % chipname
     assert os.path.exists(filename), "I could not find annotation file %s." % \
            filename
     filename = os.path.realpath(filename)
     return filename
 
+
 def find_normscript():
     from genomicode import config
-    file = "normscript.R"
-    filename = os.path.join(config.changlab_Rlib, file)
-    assert os.path.exists(filename), "I could not find normscript.R."
+    file_ = "normscript.R"
+    filename = os.path.join(config.changlab_Rlib, file_)
+    assert os.path.exists(filename), "I could not find %s." % file_
     filename = os.path.realpath(filename)
     return filename
 
+
 def main():
     import tempfile
-    from optparse import OptionParser, OptionGroup
+    from optparse import OptionParser
     from genomicode import affyio
 
     usage = (
@@ -78,17 +79,17 @@ def main():
 
     # Figure out the chip for each CEL file.
     file2chipname = {}
-    for i, file in enumerate(os.listdir(path)):
-        filename = os.path.join(path, file)
+    for i, file_ in enumerate(os.listdir(path)):
+        filename = os.path.join(path, file_)
         cn = affyio.extract_chip_name(filename)
         assert cn, "Unknown chip type: %s" % filename
-        file2chipname[file] = cn
+        file2chipname[file_] = cn
 
     # If the user specified a specific platform, then ignore all files
     # not of that platform.
-    for (file, cn) in file2chipname.items():
+    for (file_, cn) in file2chipname.items():
         if options.platform and cn != options.platform:
-            del file2chipname[file]
+            del file2chipname[file_]
     # Make sure files exist.
     assert file2chipname, "no files in path: %s" % path
 
@@ -115,11 +116,11 @@ def main():
     try:
         # Make a directory with only the CEL files with this type of annotation
         temppath = tempfile.mkdtemp(dir=".")
-        for file, cn in file2chipname.iteritems():
+        for file_, cn in file2chipname.iteritems():
             if cn != chipname:
                 continue
-            file1 = os.path.join(os.path.realpath(path), file)
-            file2 = os.path.join(temppath, file)
+            file1 = os.path.join(os.path.realpath(path), file_)
+            file2 = os.path.join(temppath, file_)
             os.symlink(file1, file2)
             #cmd = 'ln -s "%s" "%s"' % (file1, file2)
             #os.system(cmd)
@@ -135,8 +136,8 @@ def main():
     finally:
         # Clear out stuff from the temppath.
         if temppath and os.path.exists(temppath):
-            for file in os.listdir(temppath):
-                filename = os.path.join(temppath, file)
+            for file_ in os.listdir(temppath):
+                filename = os.path.join(temppath, file_)
                 os.unlink(filename)
             os.rmdir(temppath)
 
