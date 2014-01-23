6bb73fedb8eb2703f149fda173d2fcfc32e4968d
Jeffrey Chang <jeffrey.t.chang@uth.tmc.edu>
2014-01-23 17:38:44 -0600

    can now align case insensitive

--- scripts/align_matrices.py
+++ scripts/align_matrices.py
@@ -24,10 +24,11 @@ def write_matrix(filename, matrix):
     arrayio.write(matrix, open(filename, 'w'))
 
 
-def write_annot(filename, name2annots):
+def write_annot(filename, annot_data):
     from genomicode import jmath
+    name_order, name2annots = annot_data
     matrix = []
-    for name in sorted(name2annots):
+    for name in name_order:
         annots = name2annots[name]
         x = [name] + annots
         matrix.append(x)
@@ -42,12 +43,14 @@ def write_annot(filename, name2annots):
 def read_annot(filename):
     from genomicode import genesetlib
 
+    name_order = []
     name2annots = {}
     for x in genesetlib.read_tdf(
         filename, preserve_spaces=True, allow_duplicates=True):
         name, description, annots = x
+        name_order.append(name)
         name2annots[name] = annots
-    return name2annots
+    return name_order, name2annots
 
 
 def get_sample_names(matrix):
@@ -61,85 +64,126 @@ def get_sample_names(matrix):
     return x
 
 
-def find_best_annots(name2annots, match_annots):
-    # Return list of (name, matched_annotations)
-    #match_annots_s = set(match_annots)
+def find_best_annots(name2annots, match_annots, case_insensitive):
     all_matches = []  # list of (num_matches, name, matches)
     for name, annots in name2annots.iteritems():
-        matches = [x for x in annots if x in match_annots]
-        #matches = list(match_annots_s.intersection(annots))
+        matches = intersect(annots, match_annots, case_insensitive)
         x = len(matches), name, matches
         all_matches.append(x)
     all_matches = sorted(all_matches)
     x = all_matches[-1]
     x, name, matches = x
     return name, matches
+
+
+def is_same_annot(x, y, case_insensitive):
+    if case_insensitive:
+        x, y = x.upper(), y.upper()
+    return x == y
+
+
+def intersect(annots1, annots2, case_insensitive):
+    # Return the intersection of annots1 and annots2.  Preserves the
+    # order according to annots1.
+
+    ## TOO SLOW.
+    ## in_both = []
+    ## for x in annots1:
+    ##     matching = False
+    ##     for y in annots2:
+    ##         if is_same_annot(x, y, case_insensitive):
+    ##             matching = True
+    ##             break
+    ##     if matching:
+    ##         in_both.append(x)
+    ## return in_both
+    
+    annots1_cmp = annots1
+    annots2_cmp = annots2
+    if case_insensitive:
+        annots1_cmp = [x.upper() for x in annots1]
+        annots2_cmp = [x.upper() for x in annots2]
+    annots2_cmp = {}.fromkeys(annots2_cmp)
+    in_both = [
+        annots1[i] for i in range(len(annots1)) if
+        annots1_cmp[i] in annots2_cmp]
+    return in_both
+
+
+
+def annot_index(annot_list, annot, case_insensitive):
+    for i, a in enumerate(annot_list):
+        if is_same_annot(annot, a, case_insensitive):
+            break
+    else:
+        raise AssertionError, "Missing: %s" % annot
+    return i
     
 
-def find_common_samples(matrix_data, first_annot_header):
-    # First, get the common samples from all the expression data.
-    num_express = 0
-    sample2count = {}
-    for x in matrix_data:
-        infile, outfile, is_express_file, matrix = x
-        if not is_express_file:
-            continue
-        num_express += 1
-        for x in get_sample_names(matrix):
-            sample2count[x] = sample2count.get(x, 0) + 1
-    if num_express:
-        common = [s for (s, c) in sample2count.iteritems() if c == num_express]
-    if not num_express:
+def find_common_samples(matrix_data, first_annot_header, case_insensitive):
+    # Initialize the list of common samples from one of the data sets.
+    # Try one of the expression data files first.  If not available,
+    # then use an annotation file (assuming first_annot_header is
+    # provided).
+    assert matrix_data
+    common = None
+    x = [x for x in matrix_data if x[2]]  # pull out expression files
+    if x:
+        # Found an expression file.
+        infile, outfile, is_express_file, matrix = x[0]
+        common = get_sample_names(matrix)
+    else:
+        # No expression files.
         assert first_annot_header, "--first_annot_header not provided."
-        infile, outfile, is_express_file, name2annots = matrix_data[0]
+        infile, outfile, is_express_file, annot_data = matrix_data[0]
+        name_order, name2annots = annot_data
         assert first_annot_header in name2annots
         common = name2annots[first_annot_header]
-            
-    if not common:
-        return []
+    assert common
 
-    # Now, match up with the annotation files.
-    num_express = 0
-    sample2count = {}
+    # Make sure the common IDs are in each of the data sets.
     for x in matrix_data:
-        infile, outfile, is_express_file, name2annots = x
+        infile, outfile, is_express_file, data = x
         if is_express_file:
-            continue
-        name, annots = find_best_annots(name2annots, common)
-        assert annots, "%s has no matching annotations." % infile
-        common = annots
+            annots = get_sample_names(data)
+        else:
+            name_order, name2annots = annot_data
+            name, annots = find_best_annots(
+                name2annots, common, case_insensitive)
+            assert annots, "%s has no matching annotations." % infile
+
+        common = intersect(common, annots, case_insensitive)
 
     # Finally, order the annotations according to the first file given.
     assert matrix_data
     x = matrix_data[0]
-    infile, outfile, is_express_file, matrix = x
+    infile, outfile, is_express_file, data = x
     if is_express_file:
-        samples = get_sample_names(matrix)
+        samples = get_sample_names(data)
     else:
-        x, samples = find_best_annots(matrix, common)
-    samples = [x for x in samples if x in common]
+        name_order, name2annots = data
+        x, samples = find_best_annots(name2annots, common, case_insensitive)
+    samples = intersect(samples, common, case_insensitive)
     
     return samples
 
 
-def align_express(matrix, samples):
+def align_express(matrix, samples, case_insensitive):
     names = get_sample_names(matrix)
     I = []
     for n in samples:
-        assert n in names
-        i = names.index(n)
+        i = annot_index(names, n, case_insensitive)
         I.append(i)
     x = matrix.matrix(None, I)
     return x
 
 
-def align_annot(name2annots, samples):
-    name, x = find_best_annots(name2annots, samples)
+def align_annot(name2annots, samples, case_insensitive):
+    name, x = find_best_annots(name2annots, samples, case_insensitive)
     annots = name2annots[name]
     I = []
     for n in samples:
-        assert n in annots
-        i = annots.index(n)
+        i = annot_index(annots, n, case_insensitive)
         I.append(i)
     name2annots_new = {}
     for name, annots in name2annots.iteritems():
@@ -148,14 +192,16 @@ def align_annot(name2annots, samples):
     return name2annots_new
 
 
-def align_matrices(matrix_data, samples):
+def align_matrices(matrix_data, samples, case_insensitive):
     new_matrix_data = []
     for x in matrix_data:
         infile, outfile, is_express_file, data = x
         if is_express_file:
-            data_new = align_express(data, samples)
+            data_new = align_express(data, samples, case_insensitive)
         else:
-            data_new = align_annot(data, samples)
+            name_order, name2annots = data
+            x = align_annot(name2annots, samples, case_insensitive)
+            data_new = name_order, x
         x = infile, outfile, is_express_file, data_new
         new_matrix_data.append(x)
     return new_matrix_data
@@ -172,6 +218,9 @@ def main():
     parser.add_argument(
         "--clobber", default=False, action="store_true",
         help="Overwrite output files, if they already exist.")
+    parser.add_argument(
+        "--case_insensitive", default=False, action="store_true",
+        help="Do a case insensitive search of sample names.")
 
     parser.add_argument(
         "--express_file", default=[], action="append", help="")
@@ -223,11 +272,13 @@ def main():
     matrix_data = new_matrix_data
 
     # Find the intersection of each file.
-    common_samples = find_common_samples(matrix_data, args.first_annot_header)
+    common_samples = find_common_samples(
+        matrix_data, args.first_annot_header, args.case_insensitive)
     assert common_samples, "No common samples found."
 
     # Align each of the matrices.
-    matrix_data = align_matrices(matrix_data, common_samples)
+    matrix_data = align_matrices(
+        matrix_data, common_samples, args.case_insensitive)
 
     # Write out each of the matrices.
     for x in matrix_data:
@@ -240,3 +291,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    #import cProfile as profile
+    #profile.runctx("main()", globals(), locals())
