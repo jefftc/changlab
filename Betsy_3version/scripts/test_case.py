8c7543a2a2287a4d3dceafac600c5bf18317b6cd
xchen <xiaoling.chen@uth.tmc.edu>
2014-05-01 10:07:19 -0500

    fix minor bugs

--- Betsy_3version/scripts/test_case.py
+++ Betsy_3version/scripts/test_case.py
@@ -373,8 +373,8 @@ def main():
     #run_case12()
     #run_case13()
     #run_case14()
-    #run_case15()
-    run_case16()
+    run_case15()
+    #run_case16()
     #run_case17()
     
 if __name__ == '__main__':
