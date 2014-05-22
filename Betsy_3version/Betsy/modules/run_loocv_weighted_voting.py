28daf898ed6fb2b29cbca2e5f7762e10138b8083
xchen <xiaoling.chen@uth.tmc.edu>
2014-05-22 09:41:31 -0500

    update run_loocv_weighted_voting module

--- Betsy_3version/Betsy/modules/run_loocv_weighted_voting.py
+++ Betsy_3version/Betsy/modules/run_loocv_weighted_voting.py
@@ -6,65 +6,82 @@ from Betsy import bie3,rule_engine_bie3
 from Betsy import rulebase
 from Betsy import read_label_file
 from Betsy import module_utils
+from genomicode import config
+import subprocess
 
-def run(in_nodes,parameters, user_input,network):
-    data_node,cls_node = in_nodes
+
+def run(in_nodes,parameters, user_input, network):
+    data_node_train,cls_node_train = in_nodes
     outfile = name_outfile(in_nodes,user_input)
-    M = arrayio.read(data_node.identifier)
-    a,training_label,second_line = read_label_file.read(
-        cls_node.identifier)
-    full_index = range(M.ncol())
-    predict_model = __import__('Betsy.modules.' + 'classify_with_weighted_voting',globals(),
-                                 locals(),['classify_with_weighted_voting'],-2)
-    evaluate_model =  __import__('Betsy.modules.' + 'evaluate_prediction',globals(),
-                                 locals(),['evaluate_prediction'],-2)
-    f = file(outfile,'w')
-    f.write('\t'.join(['sample_name','Predicted_class','Confidence','Actual_class','Correct?']))
-    f.write('\n')
-    for i in range(M.ncol()):
-        test_index = i
-        train_index = full_index[:]
-        train_index.remove(test_index)
-        y_training = [training_label[x] for x in train_index]
-        y_test = [training_label[test_index]]
-        M_train = M.matrix(None,train_index)
-        M_test = M.matrix(None,[test_index])
-        train_file = 'train'+'_'+str(i)
-        test_file = 'test'+'_'+str(i)
-        f_out = file(train_file,'w')
-        arrayio.gct_format.write(M_train,f_out)
-        f_out.close()
-        f_out = file(test_file,'w')
-        arrayio.gct_format.write(M_test,f_out)
-        f_out.close()
-        train_label = 'train_label'+'_'+ str(i)
-        test_label = 'test_label' + '_' + str(i)
-        read_label_file.write(train_label,second_line,y_training)
-        read_label_file.write(test_label,second_line,y_test[0])
-        train_node = rulebase.SignalFile.output(format='gct',contents='class0,class1')
-        train_data = rule_engine_bie3.DataObject(train_node,identifier='train'+'_'+str(i))
-        test_node = rulebase.SignalFile.output(format='gct',contents='test')
-        test_data = rule_engine_bie3.DataObject(test_node,identifier='test'+'_'+str(i))
-        train_label_node = rulebase.ClassLabelFile.output(contents='class0,class1')
-        train_label_data = rule_engine_bie3.DataObject(train_label_node,identifier='train_label'+'_'+str(i))
-        test_label_node = rulebase.ClassLabelFile.output(contents='test')
-        test_label_data = rule_engine_bie3.DataObject(test_label_node,identifier = 'test_label'+'_'+str(i))
-        x= train_data,test_data,train_label_data
-        out_node = predict_model.run(x,parameters,user_input,network)
-        out_node_label=evaluate_model.run((out_node,test_label_data),parameters,user_input,network)
-        f1 = open(out_node_label.identifier,'r')
-        lines = f1.readlines()
-        f1.close()
-        f.write(lines[1])
-        os.remove(train_file)
-        os.remove(test_file)
-        os.remove(train_label)
-        os.remove(test_label)
-        os.remove(out_node.identifier)
-        os.remove(out_node_label.identifier)
-    f.close()
-    assert module_utils.exists_nz(outfile),(
-        'the output file %s for loocv fails'%outfile)
+    module_name = 'WeightedVotingXValidation'
+    gp_parameters = dict()
+    file1 = data_node_train.identifier
+    result, label_line, class_name = read_label_file.read(
+        cls_node_train.identifier)
+    gp_parameters['data.filename'] = file1
+    gp_parameters['class.filename'] = cls_node_train.identifier
+    
+    if 'wv_num_features' in user_input:
+        gp_parameters['num.features'] = str(user_input['wv_num_features'])
+    if 'wv_minstd' in user_input:	
+    	assert module_utils.is_number(
+            user_input['wv_minstd']), 'the sv_minstd should be number'
+        gp_parameters['min.std'] = str(user_input['wv_minstd'])
+        
+    wv_feature_stat = ['wv_snr', 'wv_ttest', 'wv_snr_median',
+                       'wv_ttest_median', 'wv_snr_minstd',
+                       'wv_ttest_minstd', 'wv_snr_median_minstd',
+                       'wv_ttest_median_minstd']
+    
+    assert parameters['wv_feature_stat'] in wv_feature_stat, (
+            'the wv_feature_stat is invalid')
+    gp_parameters['feature.selection.statistic'] = str(
+            wv_feature_stat.index(parameters[
+                'wv_feature_stat']))
+    gp_path = config.genepattern
+    gp_module = module_utils.which(gp_path)
+    assert gp_module, 'cannot find the %s' % gp_path
+    download_directory = os.path.join(os.getcwd(),'wv_result')
+    command = [gp_module, module_name, '-o', download_directory]
+    for key in gp_parameters.keys():
+        a = ['--parameters', key + ':' + gp_parameters[key]]
+        command.extend(a)
+    process = subprocess.Popen(command, shell=False,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    process.wait()
+    error_message = process.communicate()[1]
+    if error_message:
+        raise ValueError(error_message)
+    assert os.path.exists(download_directory), (
+        'there is no output directory for weightedVotingXValidation')
+    result_files = os.listdir(download_directory)
+    assert 'stderr.txt' not in result_files, 'gene_pattern get error'
+    gp_files = os.listdir(download_directory)
+    for gp_file in gp_files:
+        if gp_file.endswith('pred.odf'):
+            gp_file = os.path.join(download_directory, gp_file)
+            f = file(gp_file, 'r')
+            text = f.readlines()
+            f.close()
+            os.rename(os.path.join(download_directory, gp_file),
+                      os.path.join(download_directory, 'prediction.odf'))
+            assert text[1][0: 12] == 'HeaderLines='
+            start = int(text[1][12: -1])
+            newresult = [['Sample_name', 'Predicted_class', 'Confidence', 'Actual_class', 'Correct?']]
+            for i in text[start + 2:]:
+                line = i.split()
+                n = len(line)
+                newline = [' '.join(line[0: n - 4]), line[n - 3],
+                               line[n - 2],line[n-4],line[n-1]]
+                newresult.append(newline)
+            f = file(outfile, 'w')
+            for i in newresult:
+                f.write('\t'.join(i))
+                f.write('\n')
+            f.close()
+    assert module_utils.exists_nz(outfile), (
+        'the output file %s for run_loocv_weighted_voting fails' % outfile)
     out_node = bie3.Data(rulebase.ClassifyFile,**parameters)
     out_object = module_utils.DataObject(out_node,outfile)
     return out_object
